ac62d48516cffdbe3502c2780933cab34d7f6c7f
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-04-09 19:31:25 +0530

    learning

--- src/DSA/Array.java
+++ src/DSA/Array.java
@@ -1,14 +1,84 @@
 package DSA;
+import java.lang.StringBuffer;
+import java.util.Arrays;
 
 public class Array {
 
+
    static int missingNumber(int[] n) {
-        int c=0;
-        int s=0;
-        for (int i=0;i<n.length;i++){
-            c+=i+1;
-            s+=n[i];
+       int c = 0;
+       int s = 0;
+       for (int i = 0; i < n.length; i++) {
+           c += i + 1;
+           s += n[i];
+       }
+       return c - s;
+   }
+   String mergeAlternately(String w1, String w2) {
+       int len = Math.max(w1.length(), w2.length());
+            int i = 0;
+            StringBuffer s = new StringBuffer();
+            while (i < len) {
+                if(i<w1.length()){
+                    s.append(w1.charAt(i));
+                }
+               if(i<w2.length()) {
+                   s.append(w2.charAt(i));
+               }
+                i++;
+            }
+            return s.toString();
         }
-        return c-s;
+        int maximum(int [] arr){
+       int max=0,i=0;
+       while(i<arr.length-1){
+           if (arr[i]<arr[i+1]){
+               max=arr[i+1];
+           }
+           i++;
+       }
+       return max ;
+        }
+    int secondlarge (int[] arr){
+       int i=0,sec=0,lar=0;
+       while(i<arr.length){
+           if (arr[i]>lar){
+               sec=lar;
+               lar=arr[i];
+           }
+           if(arr[i]<lar && arr[i]>=sec){
+               sec = arr[i];
+           }
+           i++;
+
+       }
+       return sec;
+}
+
+static String reversestrsb (String word){
+       StringBuilder s = new StringBuilder(word);
+       return s.reverse().toString();
+}
+String reverewhile(String word){
+       int i=0;
+       int len= word.length();
+       char[] charar= new char[word.length()];
+       while(i<len-1){
+           charar[i] =word.charAt(len-1);
+           i++;
+           len --;
+       }
+       return new String (charar);
+}
+String leftrotate(int[] arr){
+    //[1,2,3,4,12]
+    int temp=arr[0];
+    for (int i =1; i < arr.length; i++) {
+        arr[i-1]=arr[i];
     }
+    arr[arr.length-1]=temp;
+
+    return Arrays.toString(arr) ;
+}
+
 }
--- src/DSA/Leetcode.java
+++ src/DSA/Leetcode.java
@@ -3,8 +3,13 @@ package DSA;
 public class Leetcode {
     public static void main(String[] args) {
         int[] array={3,1,4,6,5};
-        Array arr = new Array();
-        System.out.println(arr.missingNumber(array));
+      Array arr = new Array();
+        /*  System.out.println(arr.missingNumber(array));
+        System.out.println(arr.mergeAlternately("123","456"));
+        System.out.println(arr.secondlarge(new int[]{05,1,20,1}));*/
+      //  System.out.println(Array.reversesb("GOPIKA Janarthanan"));
+       // System.out.println(arr.reverewhile("hello"));
+        System.out.println(arr.leftrotate(new int[]{1,12,3,4,120,32}));
     }
 }
 
