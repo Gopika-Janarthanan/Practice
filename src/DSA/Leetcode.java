745ac304548b8ef081f320be25cf4eeee9b62756
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-04-12 17:27:52 +0530

    practing Searching

--- src/DSA/Leetcode.java
+++ src/DSA/Leetcode.java
@@ -1,13 +1,19 @@
 package DSA;
 
+import javax.sound.sampled.Line;
 import java.util.Arrays;
 
 public class Leetcode {
     public static void main(String[] args) {
         int[] arrays={13,12,40,6,50};
+        int[][] array ={{3,7,4,2},
+                {5,6,12,12},
+                {4,2,1,7,9},
+                {1,2,4,4,}};
 
       Array arr = new Array();
       LinearSearching ls=new LinearSearching();
+        System.out.println("Array :");
         /*  System.out.println(arr.missingNumber(array));
         System.out.println(arr.mergeAlternately("123","456"));
         System.out.println(arr.secondlarge(new int[]{05,1,20,1}));*/
@@ -16,7 +22,16 @@ public class Leetcode {
       //  System.out.println(arr.leftrotate(new int[]{1,12,3,4,120,32}));
     //    System.out.println(arr.leftdarr(arrays,2));
        // System.out.println(arr.leftdarr(arrays,3));
-        System.out.println(LinearSearching.evendigitsofarr(arrays));
+        System.out.println();
+        System.out.println("Linear Searching : ");
+      //  System.out.println(LinearSearching.evendigitsofarr(arrays));
+       // System.out.println(LinearSearching.maximumWealth1(array));
+        //System.out.println(LinearSearching.maximumWealth2(array));
+       // System.out.println(LinearSearching.linearTarget(arrays,12));
+     //   System.out.println(Arrays.toString(LinearSearching.linear2d(array,12)));
+      //  System.out.println();
+        System.out.println("Binary Searching  : ");
+
     }
 }
 
--- src/DSA/LinearSearching.java
+++ src/DSA/LinearSearching.java
@@ -1,6 +1,27 @@
 package DSA;
 
 public class LinearSearching {
+    static int linearTarget( int[] array, int target){
+        int j=0;
+        for(int i : array){
+            if(i==target){
+                j++;
+                return j;
+
+            }
+        }
+        return -1;
+    }
+    static int[] linear2d(int[][] array,int target){
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j< array[i].length; j++){
+                if(target==array[i][j]){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
     static int evendigitsofarr(int[]nums){
         int sum=0;
         for (int i = 0; i < nums.length; i++) {
@@ -18,5 +39,40 @@ public class LinearSearching {
             c++;
         }
         return c;
+    }
+    static int maximumWealth1(int[][] accounts) {
+        int count=0;
+        int[] arr =new int[accounts.length];
+        for(int i=0 ; i<accounts.length;i++){
+            count=0;
+            for(int j=0; j<accounts[i].length;j++){
+                count+=accounts[i][j];
+            }
+
+            arr[i]=count;
+
+        }
+        return maximum(arr);
+    }
+    static int maximum(int[] arr){
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        } return max;
+    }
+    public static int maximumWealth2(int[][] accounts) {
+        int max=0,count=0;
+        for(int[] i: accounts){
+            count=0;
+            for(int j : i){
+                count+=j;
+            }
+            if(count>max){
+                max=count;
+            }
+        }
+        return max;
     }
 }
