38438c64937a0f93cd2b962c201dd370cfdbc3fa
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-04-13 09:23:21 +0530

    binary search

--- src/DSA/BinarySearching.java
+++ src/DSA/BinarySearching.java
@@ -1,5 +1,82 @@
 package DSA;
 
 public class BinarySearching {
+    static int ceiling(int[] array, int target) {
+        int s = 0, e = array.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return array[s];
+    }
+
+    static int floor(int[] array, int target) {
+        int s = 0, e = array.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (array[mid] <= target) {
+                s = mid + 1;
+
+            } else if (array[mid] >= target) {
+                e = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return array[e];
+    }
+
+    static char findchar(char[] array, char target) {
+        int s = 0, e = array.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (array[mid] <= target) {
+                s = mid + 1;
+
+            } else {
+                e = mid - 1;
+            }
+        }
+        return array[s % array.length];
+    }
+
+    static int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        int s = binarysearch(nums, target, true);
+        int e = binarysearch(nums, target, false);
+        ans[0] = s;
+        ans[1] = e;
+        return ans;
+    }
+    static int binarysearch(int[] arr, int tar, boolean start){
+        int ans=-1;
+        int s=0,e=arr.length-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]<tar){
+                s=mid+1;
+            }
+            else if(arr[mid]>tar){
+                e=mid-1;
+            }
+            else{
+                ans=mid;
+
+                if(start){
+                    e=mid-1;
+                }
+                else{
+                    s=mid+1;
+                }
+            }
+        }
+        return ans;
+    }
 
 }
--- src/DSA/Leetcode.java
+++ src/DSA/Leetcode.java
@@ -10,27 +10,30 @@ public class Leetcode {
                 {5,6,12,12},
                 {4,2,1,7,9},
                 {1,2,4,4,}};
+        int[] binarrar= new int[]{2,4,5,12,12,12,12,12,24,38,44,55,120,122};
 
       Array arr = new Array();
       LinearSearching ls=new LinearSearching();
-        System.out.println("Array :");
-        /*  System.out.println(arr.missingNumber(array));
-        System.out.println(arr.mergeAlternately("123","456"));
-        System.out.println(arr.secondlarge(new int[]{05,1,20,1}));*/
-      //  System.out.println(Array.reversesb("GOPIKA Janarthanan"));
-       // System.out.println(arr.reverewhile("hello"));
-      //  System.out.println(arr.leftrotate(new int[]{1,12,3,4,120,32}));
-    //    System.out.println(arr.leftdarr(arrays,2));
-       // System.out.println(arr.leftdarr(arrays,3));
-        System.out.println();
-        System.out.println("Linear Searching : ");
-      //  System.out.println(LinearSearching.evendigitsofarr(arrays));
-       // System.out.println(LinearSearching.maximumWealth1(array));
-        //System.out.println(LinearSearching.maximumWealth2(array));
-       // System.out.println(LinearSearching.linearTarget(arrays,12));
-     //   System.out.println(Arrays.toString(LinearSearching.linear2d(array,12)));
-      //  System.out.println();
-        System.out.println("Binary Searching  : ");
+      System.out.println("Array :");
+      System.out.println(arr.missingNumber(arrays));
+      System.out.println(arr.mergeAlternately("123","456"));
+      System.out.println(arr.secondlarge(new int[]{05,1,20,1}));
+      System.out.println(Array.reversestrsb("GOPIKA Janarthanan"));
+      System.out.println(arr.reverewhile("hello"));
+      System.out.println(arr.leftrotate(new int[]{1,12,3,4,120,32}));
+      System.out.println();
+      System.out.println("Linear Searching : ");
+      System.out.println(LinearSearching.evendigitsofarr(arrays));
+      System.out.println(LinearSearching.maximumWealth1(array));
+      System.out.println(LinearSearching.maximumWealth2(array));
+      System.out.println(LinearSearching.linearTarget(arrays,12));
+      System.out.println(Arrays.toString(LinearSearching.linear2d(array,12)));
+      System.out.println();
+      System.out.println("Binary Searching  : ");
+      System.out.println(BinarySearching.ceiling(binarrar,15));
+      System.out.println(BinarySearching.floor(binarrar,15));
+      System.out.println(BinarySearching.findchar(new char[]{'c','f','j','h','i'},'c'));
+      System.out.println(Arrays.toString(BinarySearching.searchRange(binarrar,12)));
 
     }
 }
