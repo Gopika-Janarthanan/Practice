1e220e292585dcfb2da6300d0d1758277b983a63
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-04-28 18:54:03 +0530

    array main

---
+++ src/DSA/ArrayMain.java
@@ -0,0 +1,15 @@
+package DSA;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        Array arr = new Array();
+        int[] arrays ={1,2,34,3,5,56};
+        System.out.println("Array :");
+        System.out.println(arr.missingNumber(arrays));
+        System.out.println(arr.mergeAlternately("123","456"));
+        System.out.println(arr.secondlarge(new int[]{05,1,20,1}));
+        System.out.println(Array.reversestrsb("GOPIKA Janarthanan"));
+        System.out.println(arr.reverewhile("hello"));
+        System.out.println(arr.leftrotate(new int[]{1,12,3,4,120,32}));
+    }
+}
