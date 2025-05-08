5688873f6fb7c148dbb2251b60ca937e7b1596df
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-05-08 22:03:44 +0530

    recursion 1 practise steps

--- src/Recursion/Naturalnumbers.java
+++ src/Recursion/Naturalnumbers.java
@@ -2,8 +2,10 @@ package Recursion;
 
 public class Naturalnumbers {
     public static void main(String[] args) {
+        System.out.println(" Recursion");
         Naturalnumbers.naturalnum(12);
         System.out.println(revnaturalnumber(12));
+        System.out.println(numberOfSteps(12));
     }
     static int revnaturalnumber(int n){
         if(n==0){
@@ -19,5 +21,18 @@ public class Naturalnumbers {
         naturalnum(n-1);
         System.out.print(n +" ");
 
+    }
+    static int numberOfSteps(int num) {
+        return nums(num, 0);
+    }
+    static int nums(int num, int steps) {
+        if (num == 0) {
+            return steps;
+        }
+        if (num % 2 == 0) {
+            return nums(num / 2, steps + 1);
+        } else {
+            return nums(num - 1, steps + 1);
+        }
     }
 }
