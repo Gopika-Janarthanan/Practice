9ab27845936cd9f4e1ff611a1685c06821479713
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-06-16 22:25:03 +0530

    recursion

--- src/Recursion/Sorting.java
+++ src/Recursion/Sorting.java
@@ -1,4 +1,14 @@
 package Recursion;
 
 public class Sorting {
+    public static void main(String[] args){
+        System.out.println(  sortingrecur(0,new int[]{1,2,9,6}));
+    }
+    static boolean sortingrecur(int i,int[] arr){
+        if(arr.length-1==i){
+            return true;
+        }
+        return arr[i]<arr[i+1] && sortingrecur(i+1,arr);
+    }
+
 }
