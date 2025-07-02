704cf7b52f672f2c6ba006a94bb9d3dd192b5ed1
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-07-02 21:40:19 +0530

    patttern- half diamond with numbers

--- src/DSA/Patterns.java
+++ src/DSA/Patterns.java
@@ -2,12 +2,13 @@ package DSA;
 
 public class Patterns {
     public static void main(String[] args) {
-       rectangle(5);
+       /*rectangle(5);
        righttriangle(3);
        lefttriangle(3);
        leftno(5);
        righttno(5);
-       halfdiamond(5);
+       halfdiamond(5);*/
+       halfdiamonddnum(4);
     }
     static void rectangle(int n){
         for (int row = 0; row < n; row++) {
@@ -63,6 +64,22 @@ public class Patterns {
                 System.out.print("* ");
             }
             System.out.println();
+        }
+    }
+
+    static void halfdiamonddnum(int n){
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < row; col++) {
+                System.out.print(col+1);
+            }
+            System.out.println();
+        }
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n-r; c++) {
+                System.out.print(c+1);
+            }
+            System.out.println();
+
         }
     }
 
