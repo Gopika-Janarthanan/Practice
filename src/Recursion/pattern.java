95c02476d4f399fdb59800bbf6691bc109717675
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-07-03 19:49:29 +0530

    recusion and regex

---
+++ src/Recursion/pattern.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+public class pattern {
+    public static void main(String[] args){
+
+    }
+    static void pattern1(int n){
+
+    }
+}
---
+++ src/Regex/RegEx.java
@@ -0,0 +1,11 @@
+package Regex;
+
+public class RegEx {
+    public static void main(String[] args) {
+
+    }
+    static String removeachar(String str){
+        String ans= "kl";
+        return ans;
+    }
+}
