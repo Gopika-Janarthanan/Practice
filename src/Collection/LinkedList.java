b1cd9fb0ff93a155d5432bfd7c969bb7710c4496
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-07-10 19:03:47 +0530

    Linked List - insert

--- src/Collection/LinkedList.java
+++ src/Collection/LinkedList.java
@@ -1,121 +1,149 @@
 package Collection;
 
 public class LinkedList {
-    static Node head=null,tail=null;
-    public static void main(String[] args) {
+    static Node head = null, tail = null;
 
-        addFirst(1);
-        addFirst(2);
-        addLast(7);
+    public static void main(String[] args) {
         addFirst(3);
-        addLast(12);
+        addFirst(100);
+        addFirst(4);
+        addLast(7);
+        addFirst(2);
+        addLast(122);
         addFirst(0);
         addLast(120);
+        insert(2,12);
         countnode();
         display();
         reverse();
-        indexofnode(3);
         deletefirst();
+        indexofnode(2);
 
 
 
     }
-    static void addFirst(int val){
+
+    static void addFirst(int val) {
         Node node = new Node(val);
-        if(head==null)  {
-            head=node;
-            tail=node;
-            tail.next=null;
-        }
-        else{
-            node.next=head;
-            head=node;
+        if (head == null) {
+            head = node;
+            tail = node;
+            tail.next = null;
+        } else {
+            node.next = head;
+            head = node;
         }
     }
-    static void addLast(int val){
-        if(tail==null){
+
+    static void addLast(int val) {
+        if (tail == null) {
             addFirst(val);
-        }
-        else{
+        } else {
             Node newnode = new Node(val);
-            tail.next=newnode;
-            tail=newnode;
+            tail.next = newnode;
+            tail = newnode;
         }
     }
-    static void  display(){
-        Node current =head;
-        while(current!=null){
-            System.out.print(current.val+ " -> ");
-            current=current.next;
+    static void insert(int index,int val){
+        if(head==null){
+            tail=null;
+        }
+        else{
+            int c=0;
+            Node current=head;
+            while(current!=null){
+                c++;
+                if(index==c){
+                    Node node =new Node(val);
+                    Node temp=current.next;
+                    current.next=node;
+                    node.next=temp;
+                }
+                current =current.next;
+            }
+            display();
+        }
+    }
+
+    static void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
         }
         System.out.println("null");
     }
-    static int countnode(){
+
+    static int countnode() {
         Node current = head;
-        int c=0;
-        while(current!=null){
+        int c = 0;
+        while (current != null) {
             c++;
-            current=current.next;
+            current = current.next;
         }
         return c;
     }
-    static void reverse(){
-        Node pre=null,temp=head,after;
-        head=tail;
-        tail=temp;
-        while(temp!=null){
-            after=temp.next;
-            temp.next=pre;
-            pre=temp;
-            temp=after;
+
+    static void reverse() {
+        Node pre = null, temp = head, after;
+        head = tail;
+        tail = temp;
+        while (temp != null) {
+            after = temp.next;
+            temp.next = pre;
+            pre = temp;
+            temp = after;
         }
         display();
 
     }
-    static void deletefirst(){
-        if(head==null){
+
+    static void deletefirst() {
+        if (head == null) {
+            tail = null;
             System.out.println("Linnked list is empty");
-        }
-        else{
-            head=head.next;
+        } else {
+            head = head.next;
         }
         display();
     }
+
     static void indexofnode(int index) {
         int c = 0;
-        Node current =head;
-        while(current!=null){
-            if(c==index){
+        Node current = head;
+        while (current != null) {
+            if (c == index) {
                 System.out.println(current.val);
             }
             c++;
-            current=current.next;
+            current = current.next;
         }
+
     }
-    static void deletelast() {
+    /*static void deletelast() {
         if (tail == null) {
             System.out.println("List is empty");
         }
         else {
-            indexofnode((countnode()-1));
+            indexofnode((countnode()-2));
         }
         }
-    }
+    }*/
 
-
-class Node{
+}
+ class Node {
     Node next;
     int val;
-    Node(int val){
-        this.val=val;
-        this.next=null;
-    }
-    Node(int val,Node next){
-        this.val=val;
-        this.next=next;
+
+    Node(int val) {
+        this.val = val;
+        this.next = null;
     }
 
-
+    Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
 }
 
 
