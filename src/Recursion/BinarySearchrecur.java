13e7988ab65aa393ef192cc3b675b1fe99570a12
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-07-16 15:12:52 +0530

    Binary Search using recursion

--- src/Recursion/BinarySearchrecur.java
+++ src/Recursion/BinarySearchrecur.java
@@ -1,4 +1,23 @@
 package Recursion;
 
 public class BinarySearchrecur {
+    public static void main(String[] args) {
+        int [] arr={2,3,6,7,8,9,10,11,12,34,44,55,90};
+        bsr(0,arr.length,12,arr);
+
+    }
+    static void bsr(int s,int e, int target,int[] arr){
+        int  mid= s+(e-s)/2;
+        if(arr[mid]<target){
+            s=mid+1;
+            bsr(s,e,target,arr);
+        }
+        if(arr[mid]>target){
+            e=mid-1;
+            bsr(s,e,target,arr);
+        }
+        if(arr[mid]==target){
+            System.out.println(mid);
+        }
+    }
 }
