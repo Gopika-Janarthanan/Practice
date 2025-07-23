367f82ab1aff7fd2e63dc9f18cf41d4cc1557464
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-07-23 21:52:24 +0530

    recursion -returning Arraylist

--- src/Recursion/ArrayListRecursion.java
+++ src/Recursion/ArrayListRecursion.java
@@ -3,7 +3,9 @@ package Recursion;
 import java.util.*;
 public class ArrayListRecursion {
     public static void main(String[] args) {
-        System.out.println(findtarget(new int[]{2,3,5,5,5,8,12},0,5,new ArrayList<Integer>()));
+        int [] array = new int[]{2,3,0,5,0,5,0,0,8,12};
+        System.out.println(findtarget(array,0,5,new ArrayList<Integer>()));
+        System.out.println(findzero(array,0,new ArrayList<>()));
     }
     static ArrayList<Integer> findtarget(int [] arr,int i,int target ,ArrayList<Integer> ans){
         if(i==arr.length-1){
@@ -15,5 +17,15 @@ public class ArrayListRecursion {
         return findtarget(arr,i+1,target,ans);
 
 
+    }
+    static ArrayList<Integer> findzero(int[] arr, int index,ArrayList<Integer> ans){
+
+        if( index==arr.length-1){
+            return ans;
+        }
+        if(arr[index]== 0){
+            ans.add(index);
+        }
+        return findzero(arr,index+1,ans);
     }
 }
