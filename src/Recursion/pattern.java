d9c96a9745fe440bf7e953d7bfd0d67a9fd965b4
Gopika Janarthanan <gopikajanarthanan4@gmail.com>
2025-07-30 20:09:21 +0530

    recursion : pattern right

--- src/Recursion/pattern.java
+++ src/Recursion/pattern.java
@@ -2,7 +2,7 @@ package Recursion;
 
 public class pattern {
     public static void main(String[] args){
-        System.out.println(right(4));
+        System.out.println(right(4,0));
 
     }
     static String right(int n){
@@ -14,5 +14,18 @@ public class pattern {
         }
         System.out.println();
       return right(n-1);
+    }
+
+    static String right(int c,int r){
+        if(c<r){
+            return " " ;
+        }
+
+        if(c > r){
+            right(c,r+1);
+            System.out.print("* ");
+        }
+        System.out.println();
+        return right(c,r+1);
     }
 }
